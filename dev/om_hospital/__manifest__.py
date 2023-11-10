73171850c1e651712b3a3097cf64978c79cf8c2f
khalidalee <khalidalee@gmail.com>
2023-11-10 14:37:34 -0500

    patient form enhancements

--- dev/om_hospital/__manifest__.py
+++ dev/om_hospital/__manifest__.py
@@ -3,6 +3,7 @@
     'author': 'Khalid Ali',
     'website': 'https://nouvellesols.com',
     'summary': 'Odoo 16 Module to manage hospital facility',
+    'depends': ['mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
--- dev/om_hospital/models/patients.py
+++ dev/om_hospital/models/patients.py
@@ -3,10 +3,18 @@ from odoo import api, fields, models
 
 class HospitalPatients(models.Model):
     _name = "hospital.patient"
+    _inherit = ["mail.thread"]
     _description = "Patient Records"
 
-    name = fields.Char(String='Name', required=True)
-    age = fields.Integer(String='Age')
-    is_child = fields.Boolean(String='Is Child ?')
+    name = fields.Char(String='Name', required=True, tracking=True)
+    age = fields.Integer(String='Age', tracking=True)
+    is_child = fields.Boolean(String='Is Child ?', tracking=True)
     notes = fields.Text(String='Notes')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], String='Gender')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], String='Gender', tracking=True)
+
+    @api.onchange('age')
+    def _onchange_age(self):
+        if self.age <= 10:
+            self.is_child = True
+        else:
+            self.is_child = False
