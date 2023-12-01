19729cebcfa40ed64b6309779144ee1155418764
khalidalee <khalidalee@gmail.com>
2023-12-01 09:59:27 -0500

    from new pc

--- dev/om_hospital/models/doctors.py
+++ dev/om_hospital/models/doctors.py
@@ -1,8 +1,8 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
-
 import re
+
 
 class HospitalDoctors(models.Model):
     _name = "hospital.doctor"
@@ -12,7 +12,8 @@ class HospitalDoctors(models.Model):
 
     name = fields.Char(String='Name', required=True, tracking=True)
     reference = fields.Char(String='Reference', required=True, tracking=True)
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], String='Gender', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], String='Gender',
+                              tracking=True)
     email = fields.Char(String='Email ID', tracking=True)
 
     active = fields.Boolean(default=True)
@@ -24,12 +25,9 @@ class HospitalDoctors(models.Model):
             res.append((rec.id, name))
         return res
 
-
     @api.constrains('email_id')
     def ValidateEmail(self):
         if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", self.email) != None:
             return True
         else:
             raise ValidationError(_("Invalid Email - Please enter a valid email address !"))
-
-
